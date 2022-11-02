34fd796c2434b3996ca2820c90fe05b219ffcee6
minahilhussain <minahilh21@gmail.com>
2022-11-02 11:01:46 +0500

    added connection to seperate file

--- car-manager-backend/src/app.module.ts
+++ car-manager-backend/src/app.module.ts
@@ -1,22 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarModule } from './car/car.module';
-import { Car } from './entity/Car';
-
+import { dbConfig } from './dbConfig';
 @Module({
-  // imports: [CarModule],
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'Cars',
-      entities: [Car],
-      synchronize: true,
-    }),
-    CarModule,
-  ],
+  imports: [dbConfig, CarModule],
 })
 export class AppModule {}
