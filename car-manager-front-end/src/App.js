87d2ca9dd1bc98a6a451a4c299fad5a4b1f48d8a
minahilhussain <minahilh21@gmail.com>
2022-11-06 21:54:19 +0500

    added car component

--- car-manager-front-end/src/App.js
+++ car-manager-front-end/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Car from "./components/Car.tsx"
 function App() {
   return (
     <div>
       <h2>Car Manager</h2>
+      <ul>
+        <li><Car /></li>
+        <li><Car /></li>
+        <li><Car /></li>
+        <li><Car /></li>
+        <li><Car /></li>
+      </ul>
     </div>
   );
 }
---
+++ car-manager-front-end/src/components/Car.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Car() {
+  return (
+    <>
+      <h4>Car name</h4>
+      <p>Model</p>
+      <p>Color</p>
+    </>
+  )
+}
+
+export default Car
