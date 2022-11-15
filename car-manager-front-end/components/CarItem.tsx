39d0de050548f0beeb1b57b579b09d8de6909410
minahilhussain <minahilh21@gmail.com>
2022-11-15 11:05:32 +0500

    added content to car item

--- car-manager-front-end/components/CarItem.tsx
+++ car-manager-front-end/components/CarItem.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const CarItem = ({car}) => {
   return (
-    <>
-      
-    </>
+    <div key = {car.id}>
+      <h4>{car.name}</h4>
+      <p>{car.model}</p>
+      <p>{car.color}</p>
+    </div>
   )
 }
 
---
+++ car-manager-front-end/types/Car.ts
@@ -0,0 +1,6 @@
+export default class Car {
+  id!: String;
+  name?: String;
+  model?: String;
+  color?: String;
+}
