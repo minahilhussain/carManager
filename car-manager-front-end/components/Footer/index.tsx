6ad67e99affa133bf384dc82c40054f98a7a8194
minahilhussain <minahilh21@gmail.com>
2022-11-26 22:43:38 +0500

    added footer

---
+++ car-manager-front-end/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      &#169; Copy right CarManager 2022. All Rights Reserved.
+    </div>
+  )
+}
+
+export default Footer
