c428c3c58f1b9cad28efce4569f988f1de9fe888
minahilhussain <minahilh21@gmail.com>
2022-12-08 10:37:40 +0500

    added textfield component

---
+++ car-manager-front-end/components/TextField/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./TextField.module.css"
+
+const TextField = ({id, name, onChange}: any) => {
+  return (
+    <input 
+      type="text" 
+      id={id}
+      name={id}
+      value = {name}
+      onChange= {e=> onChange(e.target.value)} 
+      className = {styles.inputField}
+    />
+  )
+}
+
+export default TextField
