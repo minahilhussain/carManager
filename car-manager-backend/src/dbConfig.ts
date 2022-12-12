9917528826a89553e15b10d0a27000b3616cfa8a
minahilhussain <minahilh21@gmail.com>
2022-12-12 10:30:05 +0500

    added info by env variables

--- car-manager-backend/src/dbConfig.ts
+++ car-manager-backend/src/dbConfig.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Car } from './entity/Car';
+import { config } from 'dotenv';
+config();
 
 export const dbConfig = TypeOrmModule.forRoot({
   type: 'postgres',
-  host: 'localhost',
-  port: 5433,
-  username: 'postgres',
-  password: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   database: 'Cars',
   entities: [Car],
   synchronize: true,
--- car-manager-backend/src/main.ts
+++ car-manager-backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { config } from 'dotenv';
+config();
+const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
 }
 bootstrap();
