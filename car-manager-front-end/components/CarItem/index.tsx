63b817e16b411ab4a1b56fb52451799e022111f3
minahilhussain <minahilh21@gmail.com>
2022-12-13 10:36:51 +0500

    prettier updates

--- car-manager-front-end/components/CarItem/index.tsx
+++ car-manager-front-end/components/CarItem/index.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link'
-import React from 'react'
-import CarDetail from '../CarDetail'
-import styles from "./CarItem.module.css"
+import Link from 'next/link';
+import React from 'react';
+import { carProps } from '../../types/Car';
+import CarDetail from '../CarDetail';
+import styles from './CarItem.module.css';
 
-const CarItem = ({car}) => {
+const CarItem = ({ car }: carProps) => {
   return (
-    <div key = {car.uuid} className={styles.wrapper}>
-     <Link href='/car/[id]' as={`/car/${car.uuid}`}>
-        <CarDetail car={car}/>
+    <div key={car.uuid} className={styles.wrapper}>
+      <Link href="/car/[id]" as={`/car/${car.uuid}`}>
+        <CarDetail car={car} />
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default CarItem
+export default CarItem;
--- car-manager-front-end/components/Footer/index.tsx
+++ car-manager-front-end/components/Footer/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import styles from "./Footer.module.css"
+import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      &#169; Copy right CarManager 2022. All Rights Reserved.
+      &#169; Copy right CarManager {new Date().getFullYear()} All Rights
+      Reserved.
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
